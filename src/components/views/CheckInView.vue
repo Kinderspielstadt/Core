c2aa92737f23f24e0d31e4338480fe3c51f40891
Simon <simongiesel@gmail.com>
2022-07-12 22:15:30 +0200

    feat(webapp): add check in view

--- src/components/views/CheckInView.vue
+++ src/components/views/CheckInView.vue
@@ -1,5 +1,9 @@
 <template>
-  #CheckIn
+  <div class="hero h-full bg-base-200 rounded-lg">
+    <div class="hero-content text-center">
+      <h1 class="text-6xl font-bold">John Doe um 17:39:33</h1>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
