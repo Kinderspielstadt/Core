971b7607923e98ba08ff42a9e4e0e9cd0202eb49
Simon <simongiesel@gmail.com>
2022-07-30 23:13:03 +0200

    fix(webapp): anonymous session not working

--- src/components/services/appwrite.service.ts
+++ src/components/services/appwrite.service.ts
@@ -3,16 +3,14 @@ import { Client, Account } from 'appwrite';
 const sdk = new Client();
 
 sdk
-  .setEndpoint('http://192.168.178.88/v1')
+  .setEndpoint('http://192.168.1.104/v1')
   .setProject('62dfc94a1637dc23acbc');
 
 export const AppwriteService = {
   getSDK(): Client {
     // TODO Authenticate
     const account = new Account(sdk);
-    if(!account.getSession('current')) {
-      account.createAnonymousSession();
-    }
+    account.createAnonymousSession();
     return sdk;
   },
 };
